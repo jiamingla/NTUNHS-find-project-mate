182f50a3b9b26218d22c1d7e081c73968ef4cbcc
jiamingla <zopq4565@gmail.com>
2022-03-08 02:17:13 +0800

    feat: 增加後端開發所需要的檔案

--- app.py
+++ app.py
@@ -1,1 +1,31 @@
-from flask import Flask, request, abort
+from flask import Flask, request
+import configs
+
+app = Flask(__name__)
+app.config.from_object(configs.DevelopmentConfig)
+
+
+# 首頁，不限 methods
+@app.route('/')
+def index():
+    return 'Hello World'
+
+
+# POST methods(POST 大寫，要有s跟中括號)
+@app.route('/posttest', methods=['POST'])
+def posttest():
+    return 'post test success'
+
+
+@app.route('/home/<username>')
+def home_page(username):
+    return f"Hello {username}"
+
+
+@app.route('/add/<int:a>/<int:b>')
+def add(a, b):
+    return str(a + b)
+
+
+if __name__ == "__main__":
+    app.run()
